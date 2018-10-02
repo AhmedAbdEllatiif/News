57266dc5be858ea5cf362f0b381dffb0e4748c05
Ahmed <ahmedmohamedaneng@gmail.com>
2018-10-02 12:02:06 +0200

    Services_added
    Services interface_added
    AllMainSources class_added
    MainSourceItem class_added

---
+++ app/src/main/java/com/example/ahmedd/news_app/API/Model/MainModel/AllMainSources.java
@@ -0,0 +1,32 @@
+package com.example.ahmedd.news_app.API.Model.MainModel;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class AllMainSources {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("sources")
+    @Expose
+    private List<MainSourceItem> sourcesItems = null;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<MainSourceItem> getSourceItem() {
+        return getSourceItem();
+    }
+
+    public void setSources(List<MainSourceItem> sources) {
+        this.sourcesItems = sources;
+    }
+}
--- app/src/main/java/com/example/ahmedd/news_app/API/Services.java
+++ app/src/main/java/com/example/ahmedd/news_app/API/Services.java
@@ -1,4 +1,17 @@
 package com.example.ahmedd.news_app.API;
 
+import com.example.ahmedd.news_app.API.Model.MainModel.AllMainSources;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface Services {
+
+
+    @GET("sources")
+    Call<AllMainSources> allMainSources(@Query("Language") String Language,
+                                        @Query("apiKey") String apiKey
+                                        );
+    //d27e82d8021d4f08aeedc00704903264
 }
