a54d980e7f8b07966309d416d04d52e9c26d66f9
Ahmed <ahmedmohamedaneng@gmail.com>
2018-10-02 12:07:29 +0200

    APIManger class_added

--- app/src/main/java/com/example/ahmedd/news_app/API/APIManger.java
+++ app/src/main/java/com/example/ahmedd/news_app/API/APIManger.java
@@ -1,4 +1,27 @@
 package com.example.ahmedd.news_app.API;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class APIManger {
+
+
+    private static Retrofit retrofit;
+
+    private static Retrofit getInstance(){
+
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("https://newsapi.org/v2/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
+
+    public static Services getServices(){
+
+        return getInstance().create(Services.class);
+    }
 }
